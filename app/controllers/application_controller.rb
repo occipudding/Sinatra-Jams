da6427b9baa253ac9ca3662cf99cb5f29bc01314
github username <github email address>
2019-06-05 16:11:53 -0400

    add update/delete functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
+  end
+
+  patch "/artist/:id" do
+    @artist = Artist.find_by(id: params[:id])
+    @artist.update(name: params[:name], age: params[:age], bio: params[:bio])
+    redirect to "/artist/#{@artist.id}"
+  end
+
+  delete "/artist/:id" do
+    @artist = Artist.find_by(id: params[:id])
+    @artist.delete
+    redirect to "/index"
   end
 
   get "/" do
@@ -28,6 +41,11 @@ class ApplicationController < Sinatra::Base
   post "/index" do
     @artist = Artist.create(params)
     redirect to "/artist/#{@artist.id}"
+  end
+
+  get "/artist/:id/edit" do
+    @artist = Artist.find_by(id: params[:id])
+    erb :edit
   end
 
 end
